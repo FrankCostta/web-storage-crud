b50680ea449cb1a282924a22f84c5bd6a006d77c
Francisco Batista <fransta8@gmail.com>
2021-11-10 23:19:19 -0300

    Algumas alterações feitas.

--- main.js
+++ main.js
@@ -15,3 +15,32 @@ modalClose.addEventListener('click', () => {
     document.querySelector('.modal').style.display = 'none';
     inputName.value = inputPreco.value = '';
 });
+
+let id = 0;
+function setDataStorage() {
+    const name = document.querySelector('#nome').value;
+
+    if (getDataStorage() == null) {}
+    const data = getDataStorage() + name
+    
+    localStorage.setItem('database', data)
+}
+
+function getDataStorage() {
+    return localStorage.getItem('database');
+}
+
+function setTemplate(id, info) {
+    return(`
+        <div class="logbar">
+            <div class="info-bar">
+                <div class="id-box">${id}</div>
+                <div class="description-box">${info}</div>
+            </div>
+            <div class="options-bar">
+                <button class="rename"><i class="fa fa-edit"></i></button>
+                <button class="delete"><i class="fa fa-trash"></i></button>
+            </div>
+        </div>
+    `);
+}
